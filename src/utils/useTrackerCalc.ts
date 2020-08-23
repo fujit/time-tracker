8a39a55838ab3e2a5942960fbd8a80a695a1ebb5
tjifu <yzktjf04@gmail.com>
2020-08-23 14:38:48 +0900

    fix function name

--- src/utils/useTrackerCalc.ts
+++ src/utils/useTrackerCalc.ts
@@ -1,11 +1,11 @@
 import React from 'react'
 
 export const useTrackerCalc = () => {
-  const CalcSum = React.useCallback((timers: Timer[]) => {
+  const calcSum = React.useCallback((timers: Timer[]) => {
     return timers
       .filter((timer): timer is CalculatedTimer => !!timer.minute)
       .reduce((previous, current) => previous + current.minute, 0)
   }, [])
 
-  return [CalcSum]
+  return [calcSum]
 }
