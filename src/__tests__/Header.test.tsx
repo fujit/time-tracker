86873d5b830e4e2a95e9c7b702c3c27c0fc16124
fujitakazuya <88bt3ncs53n@gmail.com>
2020-09-16 10:50:25 +0900

    use jest-dom instead of DOM node attributes

--- src/__tests__/Header.test.tsx
+++ src/__tests__/Header.test.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
 
 import { Header } from '../components/Header'
 
 describe('Header', () => {
   test('renders Header Component', () => {
     render(<Header title="test" />)
-    expect(screen.getByRole('heading').textContent).toBe('test')
+    expect(screen.getByRole('heading')).toHaveTextContent('test')
   })
 })
