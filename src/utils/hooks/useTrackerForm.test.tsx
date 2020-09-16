6e1613e0dd5d8a67d1a19fdab26efa70ef67b591
fujitakazuya <88bt3ncs53n@gmail.com>
2020-09-16 10:51:19 +0900

    add test

---
+++ src/utils/hooks/useTrackerForm.test.tsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import { render, screen, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { useTrackerForm } from './useTrackerForm'
+
+const Wrapper = () => {
+  const [trackerName, isValid, setTrackerName] = useTrackerForm('initial')
+  return (
+    <>
+      <input
+        value={trackerName}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTrackerName(e.target.value)}
+      />
+      <button disabled={!isValid}>Test</button>
+    </>
+  )
+}
+
+describe('useTrackerForm', () => {
+  test('引数に渡した文字列が初期値となること', () => {
+    render(<Wrapper />)
+    expect(screen.getByRole('textbox')).toHaveValue('initial')
+  })
+
+  test('テキストボックスの変更が反映されること', () => {
+    render(<Wrapper />)
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: { value: 'FireEvent' },
+    })
+    expect(screen.getByRole('textbox')).toHaveValue('FireEvent')
+  })
+
+  test('全角スペースが半角スペースへ変換されること', () => {
+    render(<Wrapper />)
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: { value: 'Fire Event' },
+    })
+    expect(screen.getByRole('textbox')).toHaveValue('Fire Event')
+  })
+
+  test('連続する2つの半角スペースが1つの半角スペースへ変換されること', () => {
+    render(<Wrapper />)
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: { value: 'Fire  Event' },
+    })
+    expect(screen.getByRole('textbox')).toHaveValue('Fire Event')
+  })
+
+  test('複数箇所にある全角スペースがすべて半角スペースへ変換されること', () => {
+    render(<Wrapper />)
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: { value: 'Fi re E  vent' },
+    })
+    expect(screen.getByRole('textbox')).toHaveValue('Fi re E vent')
+  })
+
+  test('入力がなにもないときバリデーションエラーとなること', () => {
+    render(<Wrapper />)
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: { value: '' },
+    })
+    expect(screen.getByRole('button')).toBeDisabled()
+  })
+
+  test('入力が30文字より長い場合バリデーションエラーとなること', () => {
+    render(<Wrapper />)
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: { value: '寿限無寿限無五劫の擦切海砂利水魚の水行末雲来末風来末食う寝ると' },
+    })
+    expect(screen.getByRole('button')).toBeDisabled()
+  })
+
+  test('入力が30文字の場合バリデーションにならないこと', () => {
+    render(<Wrapper />)
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: { value: '寿限無寿限無五劫の擦切海砂利水魚の水行末雲来末風来末食う寝る' },
+    })
+    expect(screen.getByRole('button')).toBeEnabled()
+  })
+})
