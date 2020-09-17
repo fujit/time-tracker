1e8ea0718bd65267951d4abb45fef1e436341ee2
fujitakazuya <88bt3ncs53n@gmail.com>
2020-09-17 17:20:55 +0900

    fix alert style

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -5,7 +5,7 @@ type Props = {
 }
 
 export const Alert: React.FC<Props> = ({ restore }) => (
-  <div className="absolute top-0 right-0 w-1/4" role="alert">
+  <div className="absolute top-0 right-0 w-1/4 mt-4 mr-4" role="alert">
     <div className="px-4 py-3 text-red-700 bg-red-100 border border-red-400 rounded-b">
       <div className="flex items-center justify-between">
         <p>タスクを削除しました</p>
