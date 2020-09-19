1a798003b17c1a3fb792e74bd8f195ed3e0ee960
fujitakazuya <88bt3ncs53n@gmail.com>
2020-09-19 13:28:59 +0900

    fix alert design

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -5,9 +5,12 @@ type Props = {
 }
 
 export const Alert: React.FC<Props> = ({ restore }) => (
-  <div className="absolute top-0 right-0 w-1/4 mt-4 mr-4" role="alert">
-    <div className="px-4 py-3 text-red-700 bg-red-100 border border-red-400 rounded-b">
-      <div className="flex items-center justify-between">
+  <div
+    className="absolute top-0 right-0 w-1/4 mt-4 mr-4 bg-red-500 rounded transition-all duration-100"
+    role="alert"
+  >
+    <div className="px-4 py-3 text-red-700 border border-red-400 rounded">
+      <div className="flex items-center justify-between text-white">
         <p>タスクを削除しました</p>
         <button className="text-xs font-bold" onClick={restore}>
           元に戻す
