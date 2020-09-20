15594507cc95aed34e6bed61c078d2e453934776
fujitakazuya <88bt3ncs53n@gmail.com>
2020-09-20 14:49:39 +0900

    use mock

--- src/__tests__/TrackerForm.test.tsx
+++ src/__tests__/TrackerForm.test.tsx
@@ -3,28 +3,37 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 import { StateContext, DispatchContext } from '../utils/contexts/StoreContext'
-import { reducer, initialState } from '../reducer'
 import { TrackerForm } from '../components/TrackerForm'
 
-const Wrapper: React.FC<{ calculateCurrentCount: jest.Mock }> = ({ calculateCurrentCount }) => {
-  const [state, dispatch] = React.useReducer(reducer, initialState())
+type WrapperProps = {
+  calculateCurrentCount: jest.Mock
+  inProgressId?: string
+}
+
+let dispatch: jest.Mock
+const Wrapper: React.FC<WrapperProps> = (props) => {
+  const state = {
+    trackers: [],
+    inProgressId: props.inProgressId,
+  }
+  dispatch = jest.fn()
   return (
     <StateContext.Provider value={state}>
       <DispatchContext.Provider value={dispatch}>
-        <TrackerForm calculateCurrentCount={calculateCurrentCount} today="2020-09-17" />
+        <TrackerForm calculateCurrentCount={props.calculateCurrentCount} today="2020-09-17" />
       </DispatchContext.Provider>
     </StateContext.Provider>
   )
 }
 
 describe('TrackerForm', () => {
-  let calculateCurrentCount: jest.Mock
-  beforeEach(() => {
-    calculateCurrentCount = jest.fn()
-    render(<Wrapper calculateCurrentCount={calculateCurrentCount} />)
-  })
-
   describe('初期状態', () => {
+    let calculateCurrentCount: jest.Mock
+    beforeEach(() => {
+      calculateCurrentCount = jest.fn()
+      render(<Wrapper calculateCurrentCount={calculateCurrentCount} />)
+    })
+
     test('フォームは空であること', () => {
       expect(screen.getByRole('textbox')).toHaveValue('')
     })
@@ -35,6 +44,12 @@ describe('TrackerForm', () => {
   })
 
   describe('イベント', () => {
+    let calculateCurrentCount: jest.Mock
+    beforeEach(() => {
+      calculateCurrentCount = jest.fn()
+      render(<Wrapper calculateCurrentCount={calculateCurrentCount} />)
+    })
+
     test('Enter キーで計測が開始すること', () => {
       userEvent.type(screen.getByRole('textbox'), 'Test')
       expect(screen.getByRole('textbox')).toHaveValue('Test')
@@ -42,6 +57,7 @@ describe('TrackerForm', () => {
       userEvent.type(screen.getByRole('textbox'), '{enter}')
       expect(screen.getByRole('textbox')).toHaveValue('')
       expect(calculateCurrentCount).toHaveBeenCalledTimes(1)
+      expect(dispatch).toHaveBeenCalledTimes(1)
     })
 
     test('フォームに何も入力されていない場合、計測は開始しないこと', () => {
@@ -57,6 +73,7 @@ describe('TrackerForm', () => {
       expect(screen.getByRole('textbox')).toHaveValue('')
       expect(calculateCurrentCount).toHaveBeenCalledTimes(1)
       expect(screen.getByRole('img')).toHaveClass('cursor-not-allowed opacity-50')
+      expect(dispatch).toHaveBeenCalledTimes(1)
     })
 
     test('フォームに何も入力されていない場合、アイコンをクリックしても計測は開始しないこと', () => {
@@ -66,20 +83,33 @@ describe('TrackerForm', () => {
   })
 
   describe('バリデーション', () => {
-    test('計測中はフォームが非活性であること', () => {
-      userEvent.type(screen.getByRole('textbox'), 'Test{enter}')
-      expect(calculateCurrentCount).toHaveBeenCalledTimes(1)
+    describe('停止中', () => {
+      let calculateCurrentCount: jest.Mock
+      beforeEach(() => {
+        calculateCurrentCount = jest.fn()
+        render(<Wrapper calculateCurrentCount={calculateCurrentCount} />)
+      })
 
-      expect(screen.getByRole('textbox')).toBeDisabled()
+      test('31文字目以降は入力できないこと', () => {
+        userEvent.type(screen.getByRole('textbox'), 'aaaaaaaaaabbbbbbbbbbccccccccccd')
+        expect(screen.getByRole('textbox')).toHaveValue('aaaaaaaaaabbbbbbbbbbcccccccccc')
+      })
+
+      test('フォームに何も入力されていない場合、アイコンは非活性であること', () => {
+        expect(screen.getByRole('img')).toHaveClass('cursor-not-allowed opacity-50')
+      })
     })
 
-    test('31文字目以降は入力できないこと', () => {
-      userEvent.type(screen.getByRole('textbox'), 'aaaaaaaaaabbbbbbbbbbccccccccccd')
-      expect(screen.getByRole('textbox')).toHaveValue('aaaaaaaaaabbbbbbbbbbcccccccccc')
-    })
+    describe('計測中', () => {
+      let calculateCurrentCount: jest.Mock
+      beforeEach(() => {
+        calculateCurrentCount = jest.fn()
+        render(<Wrapper calculateCurrentCount={calculateCurrentCount} inProgressId="xxx" />)
+      })
 
-    test('フォームに何も入力されていない場合、アイコンは非活性であること', () => {
-      expect(screen.getByRole('img')).toHaveClass('cursor-not-allowed opacity-50')
+      test('計測中はフォームが非活性であること', () => {
+        expect(screen.getByRole('textbox')).toBeDisabled()
+      })
     })
   })
 })
--- src/__tests__/TrackerItem.test.tsx
+++ src/__tests__/TrackerItem.test.tsx
@@ -3,7 +3,6 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 import { StateContext, DispatchContext } from '../utils/contexts/StoreContext'
-// import { reducer, initialState } from '../reducer'
 import { TrackerItem } from '../components/TrackerItem'
 
 type WrapperProps = {
