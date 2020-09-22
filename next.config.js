c1950e768276d1c38b3c6a5db0b6c51cc1b8825b
fujitakazuya <88bt3ncs53n@gmail.com>
2020-09-22 18:15:22 +0900

    fetch initial data from API

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  env: {
+    apiPath: 'http://0.0.0.0:8888',
+  },
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,31 @@
+import { GetStaticProps } from 'next'
 import Layout from '../components/Layout'
 import { Tracker } from '../components/Tracker'
+import { getCurrentDay } from '../utils/DateUtil'
 
-// TODO: 本日のトラッカー一覧と日付を取得する
-const IndexPage = () => (
+type Props = {
+  todaysTrackers: Tracker[]
+  today: string
+}
+
+const IndexPage = ({ todaysTrackers, today }: Props) => (
   <Layout title="time-tracker">
-    <Tracker todaysTrackers={[]} today="2020-09-21" />
+    <Tracker todaysTrackers={todaysTrackers} today={today} />
   </Layout>
 )
+
+export const getStaticProps: GetStaticProps = async () => {
+  // const response = await fetch('http://0.0.0.0:8888/trackers')
+  const url = process.env.apiPath
+  const response = await fetch(`${url}/trackers`)
+  const todaysTrackers: Tracker[] = response.ok ? await response.json() : []
+
+  return {
+    props: {
+      todaysTrackers,
+      today: getCurrentDay(),
+    },
+  }
+}
 
 export default IndexPage
