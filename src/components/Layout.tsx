7ab44d10d431e425f982c6416bbd44321ac5b0ab
fujitakazuya <88bt3ncs53n@gmail.com>
2020-09-27 16:52:34 +0900

    fix viewport

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,7 +9,7 @@ const Layout: React.FC<Props> = ({ title = 'time-tracker', children }) => (
   <div id="app">
     <Head>
       <title>{title}</title>
-      <meta name="viewport" content="initial-scale=1.0, width-device-width" />
+      <meta name="viewport" content="initial-scale=1.0" />
     </Head>
     <Header title="time-tracker" />
     <div className="container mx-auto">{children}</div>
