97faa18cad1dc3a850dc077da989a180c8d6e8d6
fujitakazuya <88bt3ncs53n@gmail.com>
2020-10-17 12:49:14 +0900

    delete fetch wrapper

--- src/utils/Fetch.ts
+++ src/utils/Fetch.ts
@@ -1,37 +1,11 @@
-const wrapper = <T>(task: Promise<Response>): Promise<T> => {
-  return new Promise((resolve, reject) => {
-    task
-      .then((response) => {
-        if (response.ok) {
-          response
-            .json()
-            .then((json: T) => {
-              resolve(json)
-            })
-            .catch((error) => {
-              reject(error)
-            })
-        } else {
-          reject(response)
-        }
-      })
-      .catch((error) => {
-        reject(error)
-      })
+export const fetchPost = (input: RequestInfo, init?: RequestInit) => {
+  fetch(input, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    ...init,
+  }).catch((err) => {
+    console.error(err)
   })
-}
-
-export const fetchPost = <T>(input: RequestInfo, init?: RequestInit): Promise<T> =>
-  wrapper(
-    fetch(input, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-      },
-      ...init,
-    })
-  )
-
-export const fetchGet = <T>(input: RequestInfo, init?: RequestInit): Promise<T> => {
-  return wrapper(fetch(input, init))
 }
